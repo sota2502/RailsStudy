fa17f160307f61f38ef587f310a55d13073637d2
sota.nakamori <sota2502@users.noreply.github.com>
2016-05-04 06:07:42 +0000

    Handle error(404, 500)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,19 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from Exception, with: :render_internal_server_error
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActionController::RoutingError, with: :render_not_found
+
+  def render_not_found
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+  end
+
+  def render_internal_server_error
+    render file: "#{Rails.root}/public/500.html", layout: false, status: 500
+  end
+
+
   protected
 
   def configure_permitted_parameters
